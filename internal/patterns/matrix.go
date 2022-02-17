d487ab119aa24795c64890fe1a5e08b08b07618e
PepperLola <julian.orion.wright@gmail.com>
2022-02-16 16:22:34 -0800

    added cmd args for matrix line interval, custom alphabet, and word mode which makes lines go in order of the alphabet provided

--- internal/patterns/matrix.go
+++ internal/patterns/matrix.go
@@ -4,6 +4,7 @@ import (
   "fmt"
   "time"
   "math"
+  "strings"
   "math/rand"
   "golang.org/x/term"
   "github.com/PepperLola/matrix/internal/util"
@@ -13,7 +14,7 @@ type Line struct {
   x int
   y int
   length int
-  letters []rune
+  letters []string
 }
 
 var lines = []*Line{}
@@ -23,23 +24,27 @@ var MIN_LINE_GAP = 4
 var MIN_LINE_LENGTH = 30
 var MAX_LINE_LENGTH = 50
 
-var defaultCooldown = 5
-var cooldown = defaultCooldown
+var cooldown = 0
 
-var kana []rune = []rune{'ア','ァ','カ','サ','ナ','ハ','マ','ヤ','ラ','ガ','ザ','ダ','バ','パ','イ','ィ','キ','シ','チ','ニ','ヒ','ミ','リ','ヰ','ギ','ジ','ヂ','ウ','ゥ','ク','ス','ツ','ヌ','ユ','ュ','ル','グ','ヅ','プ','エ','ェ','ケ','セ','テ','ネ','ヘ','メ','レ','ゼ','デ','ベ','ペ','オ','ォ','コ','ノ','ヨ','ョ','ロ','ヲ','ゴ','ゾ','ド','ボ','ポ','ヴ','ッ','ン'}
-var latin []rune = []rune{'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}
-var numbers []rune = []rune{'0','1','2','3','4','5','6','7','8','9'}
+var kana []string = []string{"ア","ァ","カ","サ","ナ","ハ","マ","ヤ","ラ","ガ","ザ","ダ","バ","パ","イ","ィ","キ","シ","チ","ニ","ヒ","ミ","リ","ヰ","ギ","ジ","ヂ","ウ","ゥ","ク","ス","ツ","ヌ","ユ","ュ","ル","グ","ヅ","プ","エ","ェ","ケ","セ","テ","ネ","ヘ","メ","レ","ゼ","デ","ベ","ペ","オ","ォ","コ","ノ","ヨ","ョ","ロ","ヲ","ゴ","ゾ","ド","ボ","ポ","ヴ","ッ","ン"}
+var latin []string = []string{"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"}
+var numbers []string = []string{"0","1","2","3","4","5","6","7","8","9"}
 var alphabet = append(kana, append(latin, numbers...)...)
 
 func RemoveElement(slice []*Line, s int) []*Line {
     return append(slice[:s], slice[s+1:]...)
 }
 
-func RandomLetter() rune {
+func RandomLetter() string {
+  if len(alphabet) < 1 {
+    return "█"
+  } else if len(alphabet) == 1 {
+    return alphabet[0]
+  }
   return alphabet[util.RandomBetween(0, len(alphabet) - 1)]
 }
 
-func PrintMatrix(height int) {
+func PrintMatrix(height int, word bool) {
   for _, line := range lines {
     x := line.x
     y := line.y
@@ -47,21 +52,28 @@ func PrintMatrix(height int) {
       util.CursorPos(x, y - line.length)
       fmt.Print(" ")
     }
-    line.letters = append(line.letters, RandomLetter())
     // prevents overflow by printing all letters based on number of letters
     numLetters := int(math.Min(float64(line.length), float64(len(line.letters))))
+    if word {
+      line.letters = append(line.letters, alphabet[numLetters % len(alphabet)])
+    } else {
+      line.letters = append(line.letters, RandomLetter())
+    }
     for r := 0; r < numLetters; r++ {
       if y - r < height && y - r > 0 {
         util.CursorPos(x, y - r)
         color := util.CreateHSV(120, 100, 20 + int(80 * (float64(float64(numLetters - r) / float64(numLetters)))))
         fmt.Print(color.ToTrueColor())
-        fmt.Printf("%c", line.letters[r])
+        fmt.Print(line.letters[r])
       }
     }
   }
 }
 
-func TestMatrix() {
+func StartMatrix(customAlphabet string, interval int, word bool) {
+  if len(customAlphabet) > 0 {
+    alphabet = strings.Split(customAlphabet, "")
+  }
   width, height, err := term.GetSize(0)
   if err != nil {
     panic(err)
@@ -71,7 +83,7 @@ func TestMatrix() {
 
   for true {
     if cooldown <= 0 {
-      cooldown = defaultCooldown
+      cooldown = interval
       col := rand.Intn(width)
       lastLine, contains := lastLineForCol[col]
       for contains && (*lastLine).y - (*lastLine).length < MIN_LINE_GAP {
@@ -79,7 +91,7 @@ func TestMatrix() {
         lastLine, contains = lastLineForCol[col]
       }
       newLine := Line{
-        col, 0, util.RandomBetween(MIN_LINE_LENGTH, MAX_LINE_LENGTH), []rune{},
+        col, 0, util.RandomBetween(MIN_LINE_LENGTH, MAX_LINE_LENGTH), []string{},
       }
       lines = append(lines, &newLine)
       lastLineForCol[col] = &newLine
@@ -94,7 +106,7 @@ func TestMatrix() {
     }
 
     util.SaveCursor()
-    PrintMatrix(height)
+    PrintMatrix(height, word)
     util.RestoreCursor()
     time.Sleep(100 * time.Millisecond)
   }
--- main.go
+++ main.go
@@ -10,14 +10,20 @@ func main() {
   var mode string
   var path string
   var scale float64
+  var interval int
+  var alphabet string
+  var word bool
   flag.StringVar(&mode, "mode", "matrix", "Mode to run")
   flag.StringVar(&path, "path", "test.png", "File path for image")
   flag.Float64Var(&scale, "scale", -1, "Scale of image")
+  flag.IntVar(&interval, "interval", 10, "Number of frames between spawning matrix lines")
+  flag.StringVar(&alphabet, "alphabet", "", "Custom alphabet for matrix lines")
+  flag.BoolVar(&word, "word", false, "Word mode preserves order of given alphabet in matrix mode")
   flag.Parse()
 
   switch mode {
     case "matrix":
-      patterns.TestMatrix()
+      patterns.StartMatrix(alphabet, interval, word)
       break
     case "image":
       image := img.OpenImage("test.png")
